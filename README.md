ee3a88b1ccd2a4f2e9a7607f070ca93209bad45a
Ron Lipps <ronlipps@rons-imac27.attlocal.net>
2019-04-02 22:24:51 -0700

    add desktop most of tab styles and some responsive logic

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # tragic-test
-Sample header and hero banner built to impress...
+
+SDSA header and hero banner built to (hopefully) impress...
 
 Greetings to Rich and the team at Tragic Media! May this little test find you all doing well.
 
 Tools Used:
 Brackets
+Koala
 Photopea.com
-GIT
-SASS
--- js/tragic-test.js
+++ js/tragic-test.js
@@ -1,8 +1,39 @@
-// collision detection
+/******************************************
+RESPONSIVE CONTAINER CLASSING
 
-/*
-    DETECT WHEN CHILDREN COLLIDE
-*/
+reduces excessive media queries for custom 
+offset or off-grid placements by setting device data
+*******************************************/
+
+var responseTimer;
+
+function customResponse(init) {
+    // test for first run
+    init = init || false;
+    let timer = init ? 10 : 150
+    clearTimeout(responseTimer);
+    let deviceSize = ''
+    // get lower breakpoints excluding padding
+    const deviceArray = {tablet: 992, mobile: 600}
+    // get all responsive containers
+    const responsiveElements = document.querySelectorAll('[data-device]')
+    switch (true) {
+        case window.innerWidth > deviceArray['tablet']: 
+            deviceSize = 'desktop'
+            break
+        case window.innerWidth > deviceArray['mobile']:
+            deviceSize = 'tablet'
+            break
+        default:
+            deviceSize = 'mobile'
+    } 
+    // nice long timeout for performance
+    responseTimer = setTimeout( () =>  { 
+        responsiveElements.forEach( (e) => { 
+            e.setAttribute('data-device', deviceSize)
+        })
+    }, timer);
+}
 
 /******************************************
 BACKGROUND IMAGES
@@ -12,5 +43,14 @@ and applies direct child image source to it's parents background
 *******************************************/
 
 function backgrounder() {
-    
+    // find any elements with backgrounded classes
+    const elems = document.querySelectorAll('[class*="backgrounded"]') 
+    // loop through backgrounded elements
+    elems.forEach((elem) => {
+        // loop each elements children, find the image and add it to it's parents style as a background image
+        let chdrn = elem.children
+        for (x=0;x<chdrn.length;x++) {
+            if (chdrn[x].tagName == 'IMG') elem.style['backgroundImage'] = 'url('+chdrn[x].src+')'
+        }
+    })
 }
